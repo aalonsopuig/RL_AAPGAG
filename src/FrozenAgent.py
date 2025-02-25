6ebeee3b77030462f5f1c3e1761e7e7088b5e423
agm88x <andres.garcia2@carm.es>
2025-02-25 16:18:12 +0100

    P2.1 adaptar el código y crear agente

--- src/FrozenAgent.py
+++ src/FrozenAgent.py
@@ -1,4 +1,16 @@
-class FrozenAgent:
+from __future__ import annotations
+
+from collections import defaultdict
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from matplotlib.patches import Patch
+from tqdm import tqdm
+
+import gymnasium as gym
+
+class FrozenAgentBasic:
     def __init__(
         self,
         env,
@@ -29,7 +41,7 @@ class FrozenAgent:
 
         self.training_error = []
 
-    def get_action(self, env, obs: tuple[int]) -> int:
+    def get_action(self, env, state: tuple[int]) -> int:
         """
         Returns the best action with probability (1 - epsilon)
         otherwise a random action with probability epsilon to ensure exploration.
@@ -40,26 +52,152 @@ class FrozenAgent:
 
         # with probability (1 - epsilon) act greedily (exploit)
         else:
-            return int(np.argmax(self.q_values[obs]))
+            return int(np.argmax(self.q_values[state]))
 
     def update(
         self,
-        obs: tuple[int],
+        state: tuple[int],
         action: int,
         reward: float,
         terminated: bool,
-        next_obs: tuple[int],
+        next_state: tuple[int],
     ):
         """Updates the Q-value of an action."""
-        future_q_value = (not terminated) * np.max(self.q_values[next_obs])
+        future_q_value = (not terminated) * np.max(self.q_values[next_state])
         temporal_difference = (
-            reward + self.discount_factor * future_q_value - self.q_values[obs][action]
+            reward + self.discount_factor * future_q_value - self.q_values[state][action]
         )
 
-        self.q_values[obs][action] = (
-            self.q_values[obs][action] + self.lr * temporal_difference
+        self.q_values[state][action] = (
+            self.q_values[state][action] + self.lr * temporal_difference
         )
         self.training_error.append(temporal_difference)
 
     def decay_epsilon(self):
         self.epsilon = max(self.final_epsilon, self.epsilon - self.epsilon_decay)
+
+
+class FrozenAgentGreedy:
+    def __init__(
+        self,
+        env,
+        learning_rate: float,
+		initial_epsilon: float,
+        epsilon_decay: float,
+        final_epsilon: float,
+        discount_factor: float = 0.95,
+    ):
+        """Initialize a Reinforcement Learning agent with an empty dictionary
+        of state-action values (q_values), a learning rate and an epsilon.
+
+        Args:
+            learning_rate: The learning rate
+            initial_epsilon: The initial epsilon value
+            epsilon_decay: The decay for epsilon
+            final_epsilon: The final epsilon value
+            discount_factor: The discount factor for computing the Q-value
+        """
+        
+        # Matriz de valores  Q
+        self.nA = env.action_space.n
+        self.Q = np.zeros([env.observation_space.n, self.nA])
+
+        # Número de visitas. Vamos a realizar la versión incremental.
+        self.n_visits = np.zeros([env.observation_space.n, self.nA])
+
+        
+        self.q_values = defaultdict(lambda: np.zeros(self.nA))
+
+        self.lr = learning_rate
+        self.discount_factor = discount_factor
+
+        #parametros del epsilon greedy y su decaimiento
+        self.initial_epsilon = initial_epsilon
+        self.epsilon = initial_epsilon
+        self.epsilon_decay = epsilon_decay
+        self.final_epsilon = final_epsilon
+
+        self.training_error = []
+        
+        #para hacer trackiong del episodio
+        self.episode=[]
+        self.result_sum=0.0
+        self.factor=1.0
+
+        # Para mostrar la evolución en el terminal y algún dato que mostrar
+        self.stats = 0.0
+        self.list_stats = [self.stats]
+#        self.step_display = num_episodes / 10
+		
+        
+        #para hacer tracking del total de episodios
+        self.numEpisodes=0
+
+    def get_action(self, env, state: tuple[int]) -> int:
+        """
+        Returns the best action with probability (1 - epsilon)
+        otherwise a random action with probability epsilon to ensure exploration.
+        """
+        
+        return self.epsilon_greedy_policy(state)        
+
+    def updateStep(
+        self,
+        state: tuple[int],
+        action: int,
+        reward: float,
+        terminated: bool,
+        next_state: tuple[int],
+    ):
+        """Updates the Q-value of an action."""
+        self.episode.append((state, action))
+        self.result_sum += self.factor * reward
+        self.factor *= self.discount_factor
+        self.training_error.append(self.factor * reward)
+
+    def updateEpisode(self):
+        for (state, action) in self.episode:
+            self.n_visits[state, action] += 1.0
+            alpha = 1.0 / self.n_visits[state, action]
+            self.Q[state, action] += alpha * (self.result_sum - self.Q[state, action])
+        # Guardamos datos sobre la evolución
+        self.stats += self.result_sum
+        self.list_stats.append(self.stats/(self.numEpisodes+1))
+        self.numEpisodes += 1
+
+    def initEpisode(self):
+        self.episode=[]
+        self.result_sum=0.0
+        self.factor=1.0
+
+    def decay_epsilon(self):
+        #self.epsilon = min(self.final_epsilon, self.initial_epsilon/(self.numEpisodes+1))
+        self.epsilon = min(1.0, 1000.0/(self.numEpisodes+1))
+
+
+    # Política epsilon-soft. Se usa para el entrenamiento
+    def random_epsilon_greedy_policy(self, state):
+        pi_A = np.ones(self.nA, dtype=float) * self.epsilon / self.nA
+        best_action = np.argmax(self.Q[state])
+        pi_A[best_action] += (1.0 - self.epsilon)
+        return pi_A
+
+    # Política epsilon-greedy a partir de una epsilon-soft
+    def epsilon_greedy_policy(self, state):
+        pi_A = self.random_epsilon_greedy_policy(state)
+        return np.random.choice(np.arange(self.nA), p=pi_A)
+
+    # Política Greedy a partir de los valones Q. Se usa para mostrar la solución.
+    def pi_star_from_Q(self, env, Q):
+        done = False
+        pi_star = np.zeros([env.observation_space.n, env.action_space.n])
+        state, info = env.reset() # start in top-left, = 0
+        actions = ""
+        while not done:
+            action = np.argmax(Q[state, :])
+            actions += f"{action}, "
+            pi_star[state,action] = action
+            state, reward, terminated, truncated, info = env.step(action)
+            done = terminated or truncated
+        return pi_star, actions
+
--- src/on_policy_all_visit.py
+++ src/on_policy_all_visit.py
@@ -13,6 +13,16 @@ from tqdm import tqdm
 
 import gymnasium as gym
 
+import FrozenAgent
+
+import random
+
+
+semilla=100
+random.seed(semilla)
+np.random.seed(semilla)
+
+
 
 '''
 
@@ -34,209 +44,42 @@ import gymnasium as gym
 '''
 
 
-# Let's start by creating the blackjack environment.
-# Note: We are going to follow the rules from Sutton & Barto.
-# Other versions of the game can be found below for you to experiment.
-
-env = gym.make("Blackjack-v1", sab=True)
-# Other possible environment configurations are:
-
-env = gym.make('Blackjack-v1', natural=True, sab=False)
-# Whether to give an additional reward for starting with a natural blackjack, i.e. starting with an ace and ten (sum is 21).
-
-env = gym.make('Blackjack-v1', natural=False, sab=False)
-# Whether to follow the exact rules outlined in the book by Sutton and Barto. If `sab` is `True`, the keyword argument `natural` will be ignored.
-
-'''
+print("definimos el entorno:")
 #@title Importamos el lago helado
 name = 'FrozenLake-v1'
 env4 = gym.make(name, is_slippery=False, map_name="4x4", render_mode="ansi") # No resbaladizo para entender mejor los resultados.
 env8 = gym.make(name, is_slippery=False, map_name="8x8", render_mode="ansi") # No resbaladizo para entender mejor los resultados.
-'''
 
-# reset the environment to get the first observation
-done = False
-observation, info = env.reset()
+#env = gym.wrappers.RecordEpisodeStatistics(env, buffer_length=n_episodes)
 
-# observation = (16, 9, False)
+def on_policy_all_visit(agent, env, num_episodes=5000, epsilon=0.4, decay=False, discount_factor=1):
+    for episode in tqdm(range(n_episodes)):
+        state, info = env.reset(seed=semilla)
+        done = False
+#        print("========================INIT",state,info)
+    
+        #inicializo el episodio
+        agent.initEpisode()
+    
+        # play one episode
+        while not done:
+            if decay:
+                agent.decay_epsilon()
+            action = agent.get_action(env, state)
+            next_state, reward, terminated, truncated, info = env.step(action)
+ #           print(action,next_state, reward, terminated, truncated, info)
 
+            # update the agent
+            agent.updateStep(state, action, reward, terminated, next_state)
 
-# sample a random action from all valid actions
-action = env.action_space.sample()
-# action=1
+            # update if the environment is done and the current state
+            done = terminated or truncated
+            state = next_state
 
-# execute the action in our environment and receive infos from the environment
-observation, reward, terminated, truncated, info = env.step(action)
+        #después de acabar el episodio actualizo la Q y el epsilon
+        agent.updateEpisode()
+    return agent.Q, agent.list_stats
 
-# observation=(24, 10, False)
-# reward=-1.0
-# terminated=True
-# truncated=False
-# info={}
-
-
-
-
-
-# hyperparameters
-learning_rate = 0.01
-n_episodes = 100_000
-start_epsilon = 1.0
-epsilon_decay = start_epsilon / (n_episodes / 2)  # reduce the exploration over time
-final_epsilon = 0.1
-
-agent = FrozenAgent(
-    env=env,
-    learning_rate=learning_rate,
-    initial_epsilon=start_epsilon,
-    epsilon_decay=epsilon_decay,
-    final_epsilon=final_epsilon,
-)
-
-
-
-
-
-'''
-# @title Políticas del agente
-
-# actions
-LEFT, DOWN, RIGHT, UP = 0,1,2,3
-
-# Política epsilon-soft. Se usa para el entrenamiento
-def random_epsilon_greedy_policy(Q, epsilon, state, nA):
-    pi_A = np.ones(nA, dtype=float) * epsilon / nA
-    best_action = np.argmax(Q[state])
-    pi_A[best_action] += (1.0 - epsilon)
-    return pi_A
-
-# Política epsilon-greedy a partir de una epsilon-soft
-def epsilon_greedy_policy(Q, epsilon, state, nA):
-    pi_A = random_epsilon_greedy_policy(Q, epsilon, state, nA)
-    return np.random.choice(np.arange(nA), p=pi_A)
-
-# Política Greedy a partir de los valones Q. Se usa para mostrar la solución.
-def pi_star_from_Q(env, Q):
-    done = False
-    pi_star = np.zeros([env.observation_space.n, env.action_space.n])
-    state, info = env.reset() # start in top-left, = 0
-    actions = ""
-    while not done:
-        action = np.argmax(Q[state, :])
-        actions += f"{action}, "
-        pi_star[state,action] = action
-        state, reward, terminated, truncated, info = env.step(action)
-        done = terminated or truncated
-    return pi_star, actions
-'''
-
-
-env = gym.wrappers.RecordEpisodeStatistics(env, buffer_length=n_episodes)
-for episode in tqdm(range(n_episodes)):
-    obs, info = env.reset()
-    done = False
-
-    # play one episode
-    while not done:
-        action = agent.get_action(env, obs)
-        next_obs, reward, terminated, truncated, info = env.step(action)
-
-        # update the agent
-        agent.update(obs, action, reward, terminated, next_obs)
-
-        # update if the environment is done and the current obs
-        done = terminated or truncated
-        obs = next_obs
-
-    agent.decay_epsilon()
-
-
-
-
-
-
-rolling_length = 500
-fig, axs = plt.subplots(ncols=3, figsize=(12, 5))
-axs[0].set_title("Episode rewards")
-# compute and assign a rolling average of the data to provide a smoother graph
-reward_moving_average = (
-    np.convolve(
-        np.array(env.return_queue).flatten(), np.ones(rolling_length), mode="valid"
-    )
-    / rolling_length
-)
-axs[0].plot(range(len(reward_moving_average)), reward_moving_average)
-axs[1].set_title("Episode lengths")
-length_moving_average = (
-    np.convolve(
-        np.array(env.length_queue).flatten(), np.ones(rolling_length), mode="same"
-    )
-    / rolling_length
-)
-axs[1].plot(range(len(length_moving_average)), length_moving_average)
-axs[2].set_title("Training Error")
-training_error_moving_average = (
-    np.convolve(np.array(agent.training_error), np.ones(rolling_length), mode="same")
-    / rolling_length
-)
-axs[2].plot(range(len(training_error_moving_average)), training_error_moving_average)
-plt.tight_layout()
-plt.show()
-
-
-
-
-
-
-'''
-def on_policy_all_visit(env, num_episodes=5000, epsilon=0.4, decay=False, discount_factor=1):
-  # Matriz de valores  Q
-  nA = env.action_space.n
-  Q = np.zeros([env.observation_space.n, nA])
-
-  # Número de visitas. Vamos a realizar la versión incremental.
-  n_visits = np.zeros([env.observation_space.n, env.action_space.n])
-
-  # Para mostrar la evolución en el terminal y algún dato que mostrar
-  stats = 0.0
-  list_stats = [stats]
-  step_display = num_episodes / 10
-
-  for t in tqdm(range(num_episodes)):
-      state, info = env.reset(seed=100)
-      done = False
-      episode = []
-      result_sum = 0.0  # Retorno
-      factor = 1
-      while not done:
-          if decay:
-            epsilon = min(1.0, 1000.0/(t+1))
-          action = epsilon_greedy_policy(Q, epsilon, state, nA)
-          new_state, reward, terminated, truncated, info = env.step(action)
-          done = terminated or truncated
-          episode.append((state, action))
-          result_sum += factor * reward
-          factor *= discount_factor
-          state = new_state
-
-      for (state, action) in episode:
-          n_visits[state, action] += 1.0
-          alpha = 1.0 / n_visits[state, action]
-          Q[state, action] += alpha * (result_sum - Q[state, action])
-
-      # Guardamos datos sobre la evolución
-      stats += result_sum
-      list_stats.append(stats/(t+1))
-
-      # Para mostrar la evolución.  Comentar si no se quiere mostrar
-      if t % step_display == 0 and t != 0:
-          print(f"success: {stats/t}, epsilon: {epsilon}")
-
-  return Q, list_stats
-'''
-
-'''
-# @title Funciones para mostrar los resultados
 
 def plot(list_stats):
   # Creamos una lista de índices para el eje x
@@ -255,33 +98,91 @@ def plot(list_stats):
   plt.grid(True)
   plt.show()
 
-# Define la función para mostrar el tamaño de los episodios
-# Pon aquí tu código.
-'''
 
 
 
-'''
-# @title Aprendizaje
-Q, list_stats = on_policy_all_visit(env4, num_episodes=50000, epsilon=0.4, discount_factor=1)
+semilla=100
+random.seed(semilla)
+np.random.seed(semilla)
 
 
-#@title Proporción de aciertos por número de episodios
+# hyperparameters
+learning_rate = 0.01
+n_episodes = 50000
+start_epsilon = 0.4
+epsilon_decay = start_epsilon / (n_episodes / 2)  # reduce the exploration over time
+final_epsilon = 0.1
+discount_factor = 1.0
 
-plot(list_stats)
-print(f"Máxima proporcion: {list_stats[-1]}")
+agent4 = FrozenAgent.FrozenAgentGreedy(
+    env=env4,
+    learning_rate=learning_rate,
+    initial_epsilon=start_epsilon,
+    epsilon_decay=epsilon_decay,
+    final_epsilon=final_epsilon,
+    discount_factor=discount_factor,
+)
+
+Q, list_stats = on_policy_all_visit(agent4, env4, num_episodes=50000, epsilon=0.4, discount_factor=1)
+
+plot(agent4.list_stats)
+print(f"Máxima proporcion: {agent4.list_stats[-1]}")
 
 
-# @title Tabla de valores Q
 LEFT, DOWN, RIGHT, UP = 0,1,2,3
-print("Valores Q para cada estado:\n", Q)
+print("Valores Q para cada estado:\n", agent4.Q)
 
 
 
-# @title Política final
 LEFT, DOWN, RIGHT, UP = 0,1,2,3
-pi, actions = pi_star_from_Q(env4, Q)
+pi, actions = agent4.pi_star_from_Q(env4, agent4.Q)
 
 print("Política óptima obtenida\n", pi, f"\n Acciones {actions} \n Para el siguiente grid\n", env4.render())
 print()
-'''
+
+
+
+
+semilla=100
+random.seed(semilla)
+np.random.seed(semilla)
+
+
+
+env = env8
+
+
+# hyperparameters
+learning_rate = 0.01
+n_episodes = 50000
+start_epsilon = 0.4
+epsilon_decay = start_epsilon / (n_episodes / 2)  # reduce the exploration over time
+final_epsilon = 0.1
+discount_factor = 1.0
+
+agent8 = FrozenAgent.FrozenAgentGreedy(
+    env=env8,
+    learning_rate=learning_rate,
+    initial_epsilon=start_epsilon,
+    epsilon_decay=epsilon_decay,
+    final_epsilon=final_epsilon,
+    discount_factor=discount_factor,
+)
+
+
+Q, list_stats = on_policy_all_visit(agent8, env8, num_episodes=n_episodes, epsilon=0.4, decay=True, discount_factor=1)
+
+
+plot(agent8.list_stats)
+print(f"Máxima proporcion: {agent8.list_stats[-1]}")
+     
+LEFT, DOWN, RIGHT, UP = 0,1,2,3
+print("Valores Q para cada estado:\n", agent8.Q)
+
+
+LEFT, DOWN, RIGHT, UP = 0,1,2,3
+pi, actions = agent8.pi_star_from_Q(env8, agent8.Q)
+
+print("Política óptima obtenida\n", pi, f"\n Acciones {actions} \n Para el siguiente grid\n", env8.render())
+print()
+
